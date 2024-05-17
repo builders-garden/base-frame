2ad644e2706ede0a7eb762406a87313abd2b50b0
lim0n3 <simonestaffa96@gmail.com>
2024-05-17 16:12:10 +0200

    Base Frame landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import {
   appURL,
   createDebugUrl,
@@ -7,6 +6,8 @@ import {
 } from "@/lib/utils";
 import type { Metadata } from "next";
 import { fetchMetadata } from "frames.js/next";
+import { Image } from "@nextui-org/react";
+import Link from "next/link";
 
 export async function generateMetadata(): Promise<Metadata> {
   return {
@@ -22,11 +23,41 @@ export default async function Home() {
   const url = currentURL("/");
 
   return (
-    <div className="flex flex-col gap-2">
-      <h1 className="text-4xl">Base Frame</h1>
-      <Link href={createDebugUrl(url)} className="underline">
-        Debug
-      </Link>
+    <div className="p-16 h-screen flex flex-col gap-16 text-white items-center justify-center bg-gradient-to-r from-10% from-[#311E19] via-[#231844] to-[#1C2127]">
+      <div className="flex flex-col gap-2 text-center items-center justify-center mx-auto max-w-screen-md">
+        <div className="text-7xl font-black bg-clip-text bg-gradient-to-b from-70% from-white to-white/60 text-transparent">
+          Base Frame
+        </div>
+        <div className="font-semibold text-xl">
+          This is a frame following the{" "}
+          <Link
+            target="_blank"
+            href={"https://framesjs.org/guides/open-frames"}
+            className="underline"
+          >
+            frames.js OpenFrames
+          </Link>{" "}
+          standard, working on Base mainnet.
+        </div>
+      </div>
+      <div className="flex flex-col gap-2 text-center items-center justify-center mx-auto max-w-screen-md">
+        <div className="flex flex-col gap-2 items-center text-center">
+          <div className="text-3xl font-bold text-center">Usage</div>
+          <div className="text-lg sm:text-sm font-medium">
+            for more details, check the{" "}
+            <Link
+              href={"https://github.com/builders-garden/base-frame"}
+              target="_blank"
+              className="underline"
+            >
+              Github repository
+            </Link>
+          </div>
+        </div>
+        <div className="flex flex-row justify-center items-center">
+          <Image src={"/code.png"} alt="code" className="w-96" />
+        </div>
+      </div>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,14 @@
 import type { Config } from "tailwindcss";
+import { nextui } from "@nextui-org/react";
 
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // nextui
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -15,6 +19,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [nextui()],
 };
 export default config;
